6bf47216eed0ae8fa0d66409cdf1424430191bfc
Zimtir <zimtir@mail.ru>
2020-05-07 02:40:34 +0300

    add display for clicked tune

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
   import Piano from './components/Piano.svelte'
+  import Display from './components/Display.svelte'
 </script>
 
+<Display />
 <Piano />
---
+++ src/components/Display.svelte
@@ -0,0 +1,24 @@
+<script>
+  // store for clicked tune
+  import { clickedTune } from '../stores/tune.store.ts'
+</script>
+
+<style>
+  .display {
+    height: 50%;
+  }
+
+  .display__current-tune {
+    font-size: 42px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: cursive;
+  }
+</style>
+
+<div class="display">
+  <span class="display__current-tune">{$clickedTune}</span>
+</div>
