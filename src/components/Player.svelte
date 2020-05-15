e0568f552f75a4d5bb05feab250ce7f68bdd98d1
Zimtir <zimtir@mail.ru>
2020-05-16 02:26:41 +0300

    update player component to usage of store

--- src/components/Player.svelte
+++ src/components/Player.svelte
@@ -1,98 +1,117 @@
 <script>
   import { clickedTune } from '../stores/tune.store.ts'
+  import { playerConfiguration } from '../stores/player.store.ts'
   import { tick } from 'svelte'
 
   const stop = () => {
     clickedTune.set('')
-    configuration.loop = false
-  }
-
-  let configuration = {
-    loop: false,
-    timeout: 500,
-    count: 4,
-    tunes: [
-      { enabled: false, value: 'G3' },
-      { enabled: false, value: 'D#3' },
-      { enabled: false, value: 'D3' },
-      { enabled: false, value: 'C3' },
-    ],
+    $playerConfiguration.loop = false
   }
 
   const timeoutPromise = timeout => new Promise(resolve => setTimeout(resolve, timeout))
 
   const play = async () => {
-    for (let i = 0; i < configuration.count; i++) {
+    for (let i = 0; i < $playerConfiguration.count; i++) {
       // drop highlight from previous tune
       if (i !== 0) {
-        configuration.tunes[i - 1].enabled = false
+        $playerConfiguration.tunes[i - 1].enabled = false
       }
       // drop highlight at loop
-      else if (configuration.loop) {
-        configuration.tunes[configuration.count - 1].enabled = false
+      else if ($playerConfiguration.loop) {
+        $playerConfiguration.tunes[$playerConfiguration.count - 1].enabled = false
       }
 
       // play tune
-      clickedTune.set(configuration.tunes[i].value)
+      clickedTune.set($playerConfiguration.tunes[i].value)
 
       // add highlight to current tune
-      configuration.tunes[i].enabled = true
+      $playerConfiguration.tunes[i].enabled = true
 
-      await timeoutPromise(configuration.timeout)
+      await timeoutPromise($playerConfiguration.timeout)
     }
   }
 
   const add = () => {
-    configuration.count++
-    configuration.tunes.push({
+    $playerConfiguration.count++
+    $playerConfiguration.tunes.push({
       enabled: false,
       value: '',
     })
   }
 
   const remove = () => {
-    configuration.count--
-    configuration.values.pop()
+    $playerConfiguration.count--
+    $playerConfiguration.tunes.pop()
   }
 
   const loop = async () => {
-    configuration.loop = true
-    while (configuration.loop) {
+    $playerConfiguration.loop = true
+    while ($playerConfiguration.loop) {
       await play()
     }
   }
 </script>
 
 <style>
+  .player {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+  }
+
   .tune__list {
     display: flex;
     margin-bottom: 5px;
+    width: 50%;
+    flex-wrap: wrap;
+    justify-content: center;
   }
 
-  .tune__list input {
+  .tune__list input,
+  .player__actions > * {
     border: none;
     width: 80px;
     font-family: cursive;
-    font-size: 25px;
     padding: 5px;
+    font-size: 25px;
+
     text-align: center;
   }
 
   input.highlight {
     border-bottom: 1px solid #40310a;
   }
+
+  .player__actions {
+    display: flex;
+    align-items: center;
+    width: 50%;
+    height: 50px;
+    justify-content: center;
+  }
+
+  .player__actions > :not(:first-child) {
+    margin-left: 5px;
+  }
+
+  .player__actions span {
+    cursor: pointer;
+  }
 </style>
 
-<div class="tune__list">
-  {#each configuration.tunes as tune}
-    <input class:highlight={tune.enabled} bind:value={tune.value} placeholder="Fill" />
-  {/each}
+<div class="player">
+  <div class="tune__list">
+    {#each $playerConfiguration.tunes as tune}
+      <input class:highlight={tune.enabled} bind:value={tune.value} placeholder="Fill" />
+    {/each}
+  </div>
+
+  <div class="player__actions">
+    <span class="icon-plus" on:click={add} />
+    <span class="icon-minus" on:click={remove} />
+    <input bind:value={$playerConfiguration.timeout} />
+    <span class="icon-play" on:click={play} />
+    <span class="icon-stop" on:click={stop} />
+    <span class="icon-loop" on:click={loop} />
+  </div>
 </div>
-
-<input bind:value={configuration.timeout} />
-<button on:click={add}>+</button>
-<button on:click={remove}>-</button>
-
-<button on:click={play}>play</button>
-<button on:click={loop}>loop</button>
-<button on:click={stop}>stop</button>
