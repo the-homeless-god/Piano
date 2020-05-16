ef4bf8180498a7967ae19df705cb3e65132d4971
Zimtir <zimtir@mail.ru>
2020-05-16 03:17:19 +0300

    reset default configuration

--- src/stores/player.store.ts
+++ src/stores/player.store.ts
@@ -4,11 +4,6 @@ import IPlayerConfiguration from '../interfaces/player.configuration.interface'
 export const playerConfiguration: Writable<IPlayerConfiguration> = writable({
   loop: false,
   timeout: 500,
-  count: 4,
-  tunes: [
-    { enabled: false, value: 'G3' },
-    { enabled: false, value: 'D#3' },
-    { enabled: false, value: 'D3' },
-    { enabled: false, value: 'C3' },
-  ],
+  count: 0,
+  tunes: [],
 })
